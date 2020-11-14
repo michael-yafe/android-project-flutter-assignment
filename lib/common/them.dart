4a35bc6c5e441e4c62cf53d1419dc909b8d42b45
michael <you@example.com>
2020-11-14 18:39:09 +0200

    Add green Button style to common styles

--- lib/common/them.dart
+++ lib/common/them.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 final TextStyle biggerFont = const TextStyle(fontSize: 18);
 final darkRed = Colors.red[900];
+final ButtonStyle greenButton = ButtonStyle(
+  backgroundColor: MaterialStateProperty.all<Color>(Colors.green[900]),
+);
